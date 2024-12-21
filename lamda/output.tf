9e022a0604dc2e9250a0b60cecc0987cf995fa66
Rushikesh Gorte <gorterushi@gmail.com>
2024-12-21 23:06:45 +0530

    Lamda function crated

---
+++ lamda/output.tf
@@ -0,0 +1,4 @@
+output "arn" {
+    value = aws_lambda_function.test_lambda.arn
+  
+}
---
+++ lamda/variavle.tf
@@ -0,0 +1,7 @@
+variable "function_name" {
+    default = "my_lamda_function"
+    description = "This is a function name"
+}
+variable "run_time_env" {
+    default = "python3.13"
+}
