157fef4a310efd7c591d4815bb9eff000b265ef6
Rushikesh Gorte <gorterushi@gmail.com>
2024-12-23 19:06:48 +0530

    Lamda function resorces deleted

--- lamda/output.tf
+++
@@ -1,4 +0,0 @@
-output "arn" {
-    value = aws_lambda_function.test_lambda.arn
-  
-}
--- lamda/variavle.tf
+++
@@ -1,7 +0,0 @@
-variable "function_name" {
-    default = "my_lamda_function"
-    description = "This is a function name"
-}
-variable "run_time_env" {
-    default = "python3.13"
-}
